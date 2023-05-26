160237a11642711cd6f46e20052e6da8b9849327
Hariok-Q <134468110+Hariok-Q@users.noreply.github.com>
2023-05-25 23:55:00 -0300

    Initial commit

---
+++ PSWriteUtils.psm1
@@ -0,0 +1,186 @@
+﻿#region DEFAULT VARIABLE DEFINITIONS
+
+#region Default Colors
+$DefaultForegroundColor = (Get-Host).UI.RawUI.ForegroundColor
+$DefaultBackgroundColor = (Get-Host).UI.RawUI.BackgroundColor
+#endregion
+
+#region Write-Status
+$StatusMessageForegroundColor = 'White'
+$StatusDetailsForegroundColor = $defaultForegroundColor
+$StatusBackgroundColor = 'Black'
+
+$StatusInfoText = 'INFO'
+$StatusInfoForegroundColor = 'Blue'
+
+$StatusSuccessText = 'SUCESSO'
+$StatusSuccessForegroundColor = 'Green'
+
+$StatusFailText = 'FALHA'
+$StatusFailForegroundColor = 'Red'
+#endregion
+
+
+#region Write-Option
+$ValidOptionKeyColor = 'Yellow'
+$ValidOptionNameColor = $defaultForegroundColor
+$ValidOptionValueColor = $defaultForegroundColor
+
+$InvalidOptionKeyColor = 'Red'
+$InvalidOptionNameColor = $defaultForegroundColor
+$InvalidOptionValueText = '<INVÁLIDO>'
+$InvalidOptionValueColor = 'DarkGray'
+#endregion
+
+
+#region Write-Countdown
+$CountdownMessageForegroundColor = 'White'
+$CountdownSecondsForegroundColor = 'Cyan'
+#endregion
+
+#endregion
+
+
+function Write-ColorTags {
+    Param(
+        [string] $Text,
+        [switch] $NoNewLine
+    )
+
+    $tokenPattern = '(?<escape><?)(?<total><: *(?<foreground>\w*), *(?<background>\w*) *>)'
+
+    $tags = (Select-String -InputObject $Text -Pattern $tokenPattern -AllMatches).Matches
+
+    $position = 0
+    $foregroundColor = $DefaultForegroundColor
+    $backgroundColor = $DefaultBackgroundColor
+    foreach ($tag in $tags) {
+        if ($tag.Groups['escape'].Value) {
+            Write-Host $tag.Groups['total'].Value -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline
+            $position = $tag.Index + $tag.Length
+            Continue
+        }
+
+        Write-Host $Text.Substring($position, $tag.Index - $position) -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline
+
+        $position = $tag.Index + $tag.Length
+
+        if ((-not $tag.Groups['foreground'].Value) -or ($tag.Groups['foreground'].Value.ToUpper() -eq 'DEFAULT')) {
+            $foregroundColor = $DefaultForegroundColor
+        }
+        else {
+            $foregroundColor = $tag.Groups['foreground'].Value
+        }
+
+        if ((-not $tag.Groups['background'].Value) -or ($tag.Groups['background'].Value.ToUpper() -eq 'DEFAULT')) {
+            $backgroundColor = $DefaultBackgroundColor
+        }
+        else {
+            $backgroundColor = $tag.Groups['background'].Value
+        }
+    }
+    Write-Host $Text.Substring($position, $Text.Length - $position) -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline:$NoNewLine
+}
+
+function Write-Status {
+    param(
+        [Parameter(Mandatory)][ValidateSet('Info', 'Success', 'Fail')]
+        [string] $Type,
+
+        [string] $Message,
+
+        [string] $Details
+    )
+
+    $padding = (@($StatusInfoText.Length, $StatusSuccessText.Length, $StatusFailText.Length) | Measure-Object -Maximum).Maximum
+    $typeString = ''
+    $color = ''
+    switch ($Type) {
+        'Info' {
+            $typeString = $StatusInfoText
+            $color = $StatusInfoForegroundColor
+            Break
+        }
+
+        'Success' {
+            $typeString = $StatusSuccessText
+            $color = $StatusSuccessForegroundColor
+            Break
+        }
+
+        'Fail' {
+            $typeString = $StatusFailText
+            $color = $StatusFailForegroundColor
+            Break
+        }
+    }
+
+    if ($Message) {
+        $typeString = $typeString + ':'
+        $padding++
+        $messageString = ' ' + $Message.Trim()
+    }
+    $typeString = $typeString.PadRight($padding)
+
+    Write-Host $typeString -ForegroundColor $color -BackgroundColor $StatusBackgroundColor -NoNewline
+    Write-Host $messageString -ForegroundColor $StatusMessageForegroundColor -BackgroundColor $StatusBackgroundColor
+    if ($Details) {
+        foreach ($line in ($Details -split "\r?\n")) {
+            Write-Host (' '*4 + $line) -ForegroundColor $StatusDetailsForegroundColor
+        }
+    }
+}
+
+function Write-Option {
+    param(
+        [Parameter(Mandatory)]
+        [char] $Key,
+
+        [Parameter(Mandatory)]
+        [string] $Name,
+
+        [string] $CurrentValue,
+
+        [int] $Indentation = 4,
+
+        [switch] $IgnoreInvalid
+    )
+
+    $valid = $IgnoreInvalid -or ([bool] $CurrentValue)
+
+    if (-not $valid) {
+        $keyColor = $InvalidOptionKeyColor
+        $nameColor = $InvalidOptionNameColor
+        $CurrentValue = $InvalidOptionValueText
+        $currentValueColor = $InvalidOptionValueColor
+    }
+    else {
+        $keyColor = $ValidOptionKeyColor
+        $nameColor = $ValidOptionNameColor
+        $currentValueColor = $ValidOptionValueColor
+    }
+    Write-Host (' ' * $Indentation + '[') -ForegroundColor $nameColor -NoNewline
+    Write-Host $Key -ForegroundColor $keyColor -NoNewline
+    Write-Host "] $Name" -ForegroundColor $nameColor -NoNewline
+    Write-Host $CurrentValue -ForegroundColor $currentValueColor
+}
+
+function Write-Countdown {
+    Param(
+        [int] $Seconds = 5,
+        [string] $Message
+    )
+
+    if ($Message) {
+        $Message = $Message + ' '
+    }
+
+    foreach ($i in $Seconds..1) {
+        Write-Host "`r$Message" -ForegroundColor $CountdownMessageForegroundColor -NoNewline
+        Write-Host $i.ToString().PadRight($Seconds.ToString().Length) -ForegroundColor $CountdownSecondsForegroundColor -NoNewline
+        Start-Sleep -Seconds 1
+    }
+
+    Write-Host ("`r" + ' ' * ($Message.Length + $Seconds.ToString().Length + 1) + "`r") -NoNewline
+}
+
