ef58f896e463a6a09bdffa3ab3896bdfbb09ae57
Hariok-Q <134468110+Hariok-Q@users.noreply.github.com>
2023-05-30 11:40:37 -0300

    Moved Write functions to PSWriteUtils class Renamed WriteUtils class to PSWriteUtils. Moved Write-ColorTags, Write-Status, Write-Option and Write-Countdown to the class.

--- PSWriteUtils.psm1
+++ PSWriteUtils.psm1
@@ -1,281 +1,267 @@
-﻿#region DEFAULT VARIABLE DEFINITIONS
+﻿using namespace System.Text.RegularExpressions
 
-#region Default Colors
-$DefaultForegroundColor = (Get-Host).UI.RawUI.ForegroundColor
-$DefaultBackgroundColor = (Get-Host).UI.RawUI.BackgroundColor
-#endregion
-
-#region Write-Status
-$StatusMessageForegroundColor = 'White'
-$StatusDetailsForegroundColor = $defaultForegroundColor
-$StatusBackgroundColor = 'Black'
-
-$StatusInfoText = 'INFO'
-$StatusInfoForegroundColor = 'Blue'
-
-$StatusSuccessText = 'SUCESSO'
-$StatusSuccessForegroundColor = 'Green'
-
-$StatusFailText = 'FALHA'
-$StatusFailForegroundColor = 'Red'
-#endregion
-
-
-#region Write-Option
-$ValidOptionKeyColor = 'Yellow'
-$ValidOptionNameColor = $defaultForegroundColor
-$ValidOptionValueColor = $defaultForegroundColor
-
-$InvalidOptionKeyColor = 'Red'
-$InvalidOptionNameColor = $defaultForegroundColor
-$InvalidOptionValueText = '<INVÁLIDO>'
-$InvalidOptionValueColor = 'DarkGray'
-#endregion
-
-
-#region Write-Countdown
-$CountdownMessageForegroundColor = 'White'
-$CountdownSecondsForegroundColor = 'Cyan'
-#endregion
-
-#endregion
-
-class WriteUtils {
-    $Settings = [PSCustomObject]@{
-        ConsoleDefault = [PSCustomObject]@{
+class PSWriteUtils {
+    $Settings = @{
+        ConsoleDefault = @{
             ForegroundColor = 'Gray';
-            BackgroundColor = 'Black'
+            BackgroundColor = 'Default'
         };
 
-        WriteOption = [PSCustomObject]@{
+        WriteOption = @{
             Indentation = 0;
-            Valid = [PSCustomObject]@{
-                General = [PSCustomObject]@{
+            Valid = @{
+                General = @{
                     ForegroundColor = 'Gray';
-                    BackgroundColor = 'Black'
+                    BackgroundColor = 'Default'
                 };
-                Key = [PSCustomObject]@{
+                Key = @{
                     ForegroundColor = 'Yellow';
-                    BackgroundColor = 'Black'
+                    BackgroundColor = 'Default'
                 };
-                KeyBrackets = [PSCustomObject]@{
+                KeyBrackets = @{
                     ForegroundColor = 'Gray';
-                    BackgroundColor = 'Black'
+                    BackgroundColor = 'Default'
                 };
-                Value = [PSCustomObject]@{
+                Value = @{
                     ForegroundColor = 'Gray';
-                    BackgroundColor = 'Black'
+                    BackgroundColor = 'Default'
                 }
             };
-            Invalid = [PSCustomObject]@{
-                General = [PSCustomObject]@{
+            Invalid = @{
+                General = @{
                     ForegroundColor = 'DarkGray';
-                    BackgroundColor = 'Black'
+                    BackgroundColor = 'Default'
                 };
-                Key = [PSCustomObject]@{
+                Key = @{
                     ForegroundColor = 'Red';
-                    BackgroundColor = 'Black'
+                    BackgroundColor = 'Default'
                 };
-                KeyBrackets = [PSCustomObject]@{
+                KeyBrackets = @{
                     ForegroundColor = 'Gray';
-                    BackgroundColor = 'Black'
+                    BackgroundColor = 'Default'
                 };
-                Value = [PSCustomObject]@{
+                Value = @{
                     ForegroundColor = 'DarkGray';
-                    BackgroundColor = 'Black';
+                    BackgroundColor = 'Default';
                     Text = '<INVALID>'
                 }
             }
         };
 
-        WriteStatus = [PSCustomObject]@{
+        WriteStatus = @{
             Indentation = 0;
-            Message = [PSCustomObject]@{
+            Message = @{
                 ForegroundColor = 'White';
                 BackgroundColor = 'Black'
             };
-            Type = [PSCustomObject]@{
-                Info = [PSCustomObject]@{
+            Type = @{
+                Info = @{
                     Text = 'INFO';
                     ForegroundColor = 'Blue';
                     BackgroundColor = 'Black'
                 };
-                Success = [PSCustomObject]@{
+                Success = @{
                     Text = 'SUCCESS';
                     ForegroundColor = 'Green';
                     BackgroundColor = 'Black'
                 };
-                Fail = [PSCustomObject]@{
+                Fail = @{
                     Text = 'FAIL';
                     ForegroundColor = 'Red';
                     BackgroundColor = 'Black'
                 }
             };
-            Details = [PSCustomObject]@{
+            Details = @{
                 Indentation = 4;
                 ForegroundColor = 'Gray';
-                BackgroundColor = 'Black'
+                BackgroundColor = 'Default'
             }
         };
 
-        WriteCountdown = [PSCustomObject]@{
+        WriteCountdown = @{
             Indentation = 0;
-            Message = [PSCustomObject]@{
+            Message = @{
                 Text = 'Waiting... ';
                 ForegroundColor = 'White';
-                BackgroundColor = 'Black'
+                BackgroundColor = 'Default'
             };
-            Seconds = [PSCustomObject]@{
+            Seconds = @{
+                Amount = 5;
                 ForegroundColor = 'Cyan';
-                BackgroundColor = 'Black'
+                BackgroundColor = 'Default'
+            }
+        }
+    }
+    $ColorTagsRegex = [Regex]::new(
+        '(?<escape><)?(?<total><: *(?<foreground>\w*), *(?<background>\w*) *>)',
+        [RegexOptions]::Compiled -bor [RegexOptions]::IgnoreCase
+    )
+
+    PSWriteUtils() {
+        $this.InitializeSettings($this.Settings)
+    }
+
+    [void] InitializeSettings([hashtable]$Settings) {
+        $keys = [array]$Settings.Keys
+        for ($i = 0; $i -lt $keys.Count; $i++) {
+            $key = $keys[$i]
+            $value = $Settings[$key]
+            if ($value -is [hashtable]) {
+                $this.InitializeSettings($value)
+                continue
+            }
+
+            if ($value -eq 'default') {
+                if ($key -like "*foreground*") {
+                    $Settings[$key] = (Get-Host).UI.RawUI.ForegroundColor
+                }
+                elseif ($key -like "*background*") {
+                    $Settings[$key] = (Get-Host).UI.RawUI.BackgroundColor
+                }
             }
         }
     }
 
-    WriteUtils() {
+    [void] WriteColorTags([string] $Text, [bool] $NoNewLine) {
+        $tagMatches = $this.ColorTagsRegex.Matches($Text)
+
+        $position = 0
+        $foregroundColor = $this.Settings.ConsoleDefault.ForegroundColor
+        $backgroundColor = $this.Settings.ConsoleDefault.BackgroundColor
+        foreach ($tag in $tagMatches) {
+            Write-Host $Text.Substring($position, $tag.Index - $position) -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline
+            $position = $tag.Index + $tag.Length
+
+            if ($tag.Groups['escape'].Value) {
+                Write-Host $tag.Groups['total'].Value -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline
+                continue
+            }
+
+            if ((-not $tag.Groups['foreground'].Value) -or ($tag.Groups['foreground'].Value -eq 'default')) {
+                $foregroundColor = $this.Settings.ConsoleDefault.ForegroundColor
+            }
+            else {
+                $foregroundColor = $tag.Groups['foreground'].Value
+            }
+
+            if ((-not $tag.Groups['background'].Value) -or ($tag.Groups['background'].Value -eq 'default')) {
+                $backgroundColor = $this.Settings.ConsoleDefault.BackgroundColor
+            }
+            else {
+                $backgroundColor = $tag.Groups['background'].Value
+            }
+        }
+        Write-Host $Text.Substring($position, $Text.Length - $position) -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline:$NoNewLine
     }
-}
 
+    [void] WriteStatus([string] $Type, [string] $Message, [string] $Details) {
+        $padding = (@(
+            $this.Settings.WriteStatus.Type.Info.Text.Length,
+            $this.Settings.WriteStatus.Type.Success.Text.Length,
+            $this.Settings.WriteStatus.Type.Fail.Text.Length
+        ) | Measure-Object -Maximum).Maximum
+        $Message = $Message.Trim()
+        $Details = $Details.Trim()
 
+        $typeString = ''
+        $foregroundColor = ''
+        $backgroundColor = ''
+        switch ($Type) {
+            'Info' {
+                $typeString = $this.Settings.WriteStatus.Type.Info.Text
+                $foregroundColor = $this.Settings.WriteStatus.Type.Info.ForegroundColor
+                $backgroundColor = $this.Settings.WriteStatus.Type.Info.BackgroundColor
+                Break
+            }
 
-function Write-ColorTags {
-    Param(
-        [string] $Text,
-        [switch] $NoNewLine
-    )
+            'Success' {
+                $typeString = $this.Settings.WriteStatus.Type.Success.Text
+                $foregroundColor = $this.Settings.WriteStatus.Type.Success.ForegroundColor
+                $backgroundColor = $this.Settings.WriteStatus.Type.Success.BackgroundColor
+                Break
+            }
 
-    $tokenPattern = '(?<escape><)?(?<total><: *(?<foreground>\w*), *(?<background>\w*) *>)'
+            'Fail' {
+                $typeString = $this.Settings.WriteStatus.Type.Fail.Text
+                $foregroundColor = $this.Settings.WriteStatus.Type.Fail.ForegroundColor
+                $backgroundColor = $this.Settings.WriteStatus.Type.Fail.BackgroundColor
+                Break
+            }
 
-    $tags = (Select-String -InputObject $Text -Pattern $tokenPattern -AllMatches).Matches
-
-    $position = 0
-    $foregroundColor = $DefaultForegroundColor
-    $backgroundColor = $DefaultBackgroundColor
-    foreach ($tag in $tags) {
-        Write-Host $Text.Substring($position, $tag.Index - $position) -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline
-
-        if ($tag.Groups['escape'].Value) {
-            Write-Host $tag.Groups['total'].Value -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline
+            Default {
+                throw [System.Management.Automation.MethodException] "Invalid Type value. It must be one of the following strings: 'Info', 'Success', 'Fail'."
+            }
         }
 
-        $position = $tag.Index + $tag.Length
+        if ($Message) {
+            $typeString = $typeString + ':'
+            $padding++
+            $Message = ' ' + $Message
+        }
+        $typeString = $typeString.PadRight($padding)
 
-        if ((-not $tag.Groups['foreground'].Value) -or ($tag.Groups['foreground'].Value.ToUpper() -eq 'DEFAULT')) {
-            $foregroundColor = $DefaultForegroundColor
+        Write-Host (' ' * $this.Settings.WriteStatus.Indentation) -NoNewline
+        Write-Host $typeString -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline
+        Write-Host $Message -ForegroundColor $this.Settings.WriteStatus.Message.ForegroundColor -BackgroundColor $this.Settings.WriteStatus.Message.BackgroundColor
+        if ($Details) {
+            foreach ($line in ($Details -split "\r?\n")) {
+                Write-Host (' ' * $this.Settings.WriteStatus.Details.Indentation) -NoNewline
+                Write-Host $line -ForegroundColor $this.Settings.WriteStatus.Details.ForegroundColor -BackgroundColor $this.Settings.WriteStatus.Details.BackgroundColor
+            }
+        }
+    }
+
+    [void] WriteOption([string] $Key, [string] $Name, [string] $CurrentValue, [bool] $IgnoreInvalid) {
+        $valid = $IgnoreInvalid -or ([bool] $CurrentValue)
+
+        if (-not $valid) {
+            $keyForeground = $this.Settings.WriteOption.Invalid.Key.ForegroundColor
+            $keyBackground = $this.Settings.WriteOption.Invalid.Key.BackgroundColor
+            $keyBracketsForeground = $this.Settings.WriteOption.Invalid.KeyBrackets.ForegroundColor
+            $keyBracketsBackground = $this.Settings.WriteOption.Invalid.KeyBrackets.BackgroundColor
+            $generalForeground = $this.Settings.WriteOption.Invalid.General.ForegroundColor
+            $generalBackground = $this.Settings.WriteOption.Invalid.General.BackgroundColor
+            $CurrentValue = $this.Settings.WriteOption.Invalid.Value.Text
+            $CurrentValueForeground = $this.Settings.WriteOption.Invalid.Value.ForegroundColor
+            $CurrentValueBackground = $this.Settings.WriteOption.Invalid.Value.BackgroundColor
         }
         else {
-            $foregroundColor = $tag.Groups['foreground'].Value
+            $keyForeground = $this.Settings.WriteOption.Valid.Key.ForegroundColor
+            $keyBackground = $this.Settings.WriteOption.Valid.Key.BackgroundColor
+            $keyBracketsForeground = $this.Settings.WriteOption.Valid.KeyBrackets.ForegroundColor
+            $keyBracketsBackground = $this.Settings.WriteOption.Valid.KeyBrackets.BackgroundColor
+            $generalForeground = $this.Settings.WriteOption.Valid.General.ForegroundColor
+            $generalBackground = $this.Settings.WriteOption.Valid.General.BackgroundColor
+            $CurrentValueForeground = $this.Settings.WriteOption.Valid.Value.ForegroundColor
+            $CurrentValueBackground = $this.Settings.WriteOption.Valid.Value.BackgroundColor
+        }
+        Write-Host (' ' * $this.Settings.WriteOption.Indentation) -NoNewline
+        Write-Host '[' -ForegroundColor $keyBracketsForeground -BackgroundColor $keyBracketsBackground -NoNewline
+        Write-Host $Key -ForegroundColor $keyForeground -BackgroundColor $keyBackground -NoNewline
+        Write-Host ']' -ForegroundColor $keyBracketsForeground -BackgroundColor $keyBracketsBackground -NoNewline
+        Write-Host " $Name " -ForegroundColor $generalForeground -BackgroundColor $generalBackground -NoNewline
+        Write-Host $CurrentValue -ForegroundColor $CurrentValueForeground -BackgroundColor $CurrentValueBackground
+    }
+
+    [void] WriteCountdown([string] $Message, [int] $Seconds) {
+        if (-not $Message) {
+            $Message = $this.Settings.WriteCountdown.Message.Text
+        }
+        if ($Seconds -lt 1) {
+            $Seconds = $this.Settings.WriteCountdown.Seconds.Amount
         }
 
-        if ((-not $tag.Groups['background'].Value) -or ($tag.Groups['background'].Value.ToUpper() -eq 'DEFAULT')) {
-            $backgroundColor = $DefaultBackgroundColor
+        foreach ($i in $Seconds..1) {
+            Write-Host ("`r" + (' ' * $this.Settings.WriteCountdown.Indentation)) -NoNewline
+            Write-Host $Message -ForegroundColor $this.Settings.WriteCountdown.Message.ForegroundColor -BackgroundColor $this.Settings.WriteCountdown.Message.BackgroundColor -NoNewline
+            Write-Host $i.ToString().PadRight($Seconds.ToString().Length) -ForegroundColor $this.Settings.WriteCountdown.Seconds.ForegroundColor -BackgroundColor $this.Settings.WriteCountdown.Seconds.BackgroundColor -NoNewline
+            Start-Sleep -Seconds 1
         }
-        else {
-            $backgroundColor = $tag.Groups['background'].Value
-        }
+    
+        Write-Host ("`r" + ' ' * ($this.Settings.WriteCountdown.Indentation + $Message.Length + $Seconds.ToString().Length + 1) + "`r") -NoNewline
     }
-    Write-Host $Text.Substring($position, $Text.Length - $position) -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor -NoNewline:$NoNewLine
+
 }
 
-function Write-Status {
-    param(
-        [Parameter(Mandatory)][ValidateSet('Info', 'Success', 'Fail')]
-        [string] $Type,
-
-        [string] $Message,
-
-        [string] $Details
-    )
-
-    $padding = (@($StatusInfoText.Length, $StatusSuccessText.Length, $StatusFailText.Length) | Measure-Object -Maximum).Maximum
-    $typeString = ''
-    $color = ''
-    switch ($Type) {
-        'Info' {
-            $typeString = $StatusInfoText
-            $color = $StatusInfoForegroundColor
-            Break
-        }
-
-        'Success' {
-            $typeString = $StatusSuccessText
-            $color = $StatusSuccessForegroundColor
-            Break
-        }
-
-        'Fail' {
-            $typeString = $StatusFailText
-            $color = $StatusFailForegroundColor
-            Break
-        }
-    }
-
-    if ($Message) {
-        $typeString = $typeString + ':'
-        $padding++
-        $messageString = ' ' + $Message.Trim()
-    }
-    $typeString = $typeString.PadRight($padding)
-
-    Write-Host $typeString -ForegroundColor $color -BackgroundColor $StatusBackgroundColor -NoNewline
-    Write-Host $messageString -ForegroundColor $StatusMessageForegroundColor -BackgroundColor $StatusBackgroundColor
-    if ($Details) {
-        foreach ($line in ($Details -split "\r?\n")) {
-            Write-Host (' '*4 + $line) -ForegroundColor $StatusDetailsForegroundColor
-        }
-    }
-}
-
-function Write-Option {
-    param(
-        [Parameter(Mandatory)]
-        [string] $Key,
-
-        [Parameter(Mandatory)]
-        [string] $Name,
-
-        [string] $CurrentValue,
-
-        [int] $Indentation = 4,
-
-        [switch] $IgnoreInvalid
-    )
-
-    $valid = $IgnoreInvalid -or ([bool] $CurrentValue)
-
-    if (-not $valid) {
-        $keyColor = $InvalidOptionKeyColor
-        $nameColor = $InvalidOptionNameColor
-        $CurrentValue = $InvalidOptionValueText
-        $currentValueColor = $InvalidOptionValueColor
-    }
-    else {
-        $keyColor = $ValidOptionKeyColor
-        $nameColor = $ValidOptionNameColor
-        $currentValueColor = $ValidOptionValueColor
-    }
-    Write-Host (' ' * $Indentation + '[') -ForegroundColor $nameColor -NoNewline
-    Write-Host $Key -ForegroundColor $keyColor -NoNewline
-    Write-Host "] $Name" -ForegroundColor $nameColor -NoNewline
-    Write-Host $CurrentValue -ForegroundColor $currentValueColor
-}
-
-function Write-Countdown {
-    Param(
-        [int] $Seconds = 5,
-        [string] $Message
-    )
-
-    if ($Message) {
-        $Message = $Message + ' '
-    }
-
-    foreach ($i in $Seconds..1) {
-        Write-Host "`r$Message" -ForegroundColor $CountdownMessageForegroundColor -NoNewline
-        Write-Host $i.ToString().PadRight($Seconds.ToString().Length) -ForegroundColor $CountdownSecondsForegroundColor -NoNewline
-        Start-Sleep -Seconds 1
-    }
-
-    Write-Host ("`r" + ' ' * ($Message.Length + $Seconds.ToString().Length + 1) + "`r") -NoNewline
-}
-
+$PSWriteUtils = [PSWriteUtils]::new()
+Export-ModuleMember -Function * -Alias * -Variable PSWriteUtils
