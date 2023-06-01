55f30e3aec4fc1d44ecbd39690733d4fb39923e7
Hariok-Q <134468110+Hariok-Q@users.noreply.github.com>
2023-06-01 09:37:05 -0300

    Added WriteCountdown test

--- Tests/PSWriteUtils.WriteCountdown.ps1
+++ Tests/PSWriteUtils.WriteCountdown.ps1
@@ -1,7 +1,8 @@
 using module '..\PSWriteUtils.psm1'
 
 Write-Host ''
-$PSWriteUtils.WriteColorTags("<:black, white>WriteCountdown: Default settings", $false)
+$PSWriteUtils.WriteColorTags("<:black, white>WriteCountdown: Default settings ", $false)
+$PSWriteUtils.WriteCountdown("", -1, -1)
 $PSWriteUtils.WriteCountdown("This is not the default message...", 3, 0)
 $PSWriteUtils.WriteCountdown("", 3, -1)
 $PSWriteUtils.WriteCountdown("", -1, 8)
